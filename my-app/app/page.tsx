abd34fdee7eead9742ff5b109588a1711d2feade
Mileslegend <140729768+Mileslegend@users.noreply.github.com>
2026-02-24 13:16:43 +0000

    lets see

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,19 +1,21 @@
 'use client'
 
-//import AboutUs2 from "@/components/mvpblocks/about-us-2";
+import AboutUs1 from "@/components/mvpblocks/about-us-1";
 import AppHero from "@/components/mvpblocks/app-hero";
 import ContactUs1 from "@/components/mvpblocks/contact-us-1";
 import Footer4Col from "@/components/mvpblocks/footer-4col";
 import Header1 from "@/components/mvpblocks/header-1";
-//import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <main>
+    <main >
     <Header1 />
     <AppHero />
-    {/* <AboutUs2 /> */}
+    <AboutUs1 />
+    
     <ContactUs1 />
+    
     <Footer4Col />
     </main>
   );
